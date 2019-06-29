2cf90b5d9e353ea26244038f9972b939de2f7a6b
Sebastian DÅÃ DÃoell <sebastian@katallaxie.me>
2019-06-29 17:10:28 +0200

    wip: publish func

--- pkg/plugins/plugin.go
+++ pkg/plugins/plugin.go
@@ -8,16 +8,28 @@ import (
 	"github.com/nats-io/stan.go"
 )
 
+// SubscribeFunc ...
+type SubscribeFunc = func(env *Env, sub <-chan *pb.Event, exit <-chan struct{}) func() error
+
+// PublishFunc ...
+type PublishFunc = func(env *Env, sub <-chan *pb.Event, exit <-chan struct{}) func() error
+
+var (
+	// DefaultSubscribeFunc ...
+	DefaultSubscribeFunc = subFunc
+	// DefaultPublishFunc ...
+	DefaultPublishFunc = pubFunc
+)
+
 // Plugin ...
 type Plugin interface {
 	Subscribe() <-chan *pb.Event
+	Publish() chan<- *pb.Event
 	Wait() error
 }
 
 type plugin struct {
 	env *Env
-
-	sub chan *pb.Event
 
 	exit    chan struct{}
 	errOnce sync.Once
@@ -30,45 +42,29 @@ func New(e *Env) Plugin {
 	p := new(plugin)
 
 	p.env = e
-	p.sub = make(chan *pb.Event)
 
 	return p
 }
 
+// Subscribe ...
 func (p *plugin) Subscribe() <-chan *pb.Event {
 	sub := make(chan *pb.Event)
 
-	p.run(p.subFunc(sub))
+	p.run(DefaultSubscribeFunc(p.env, sub, p.exit))
 
 	return sub
 }
 
-func (p *plugin) subFunc(sub chan *pb.Event) func() error {
-	return func() error {
-		sc, err := stan.Connect(p.env.Get(AutobotClusterID), p.env.Get(AutobotClusterURL))
-		if err != nil {
-			return err
-		}
-		defer sc.Close()
+// Publish ...
+func (p *plugin) Publish() chan<- *pb.Event {
+	pub := make(chan *pb.Event)
 
-		s, err := sc.Subscribe(p.env.Get(AutobotTopic), func(m *stan.Msg) {
-			sub <- &pb.Event{}
-		})
-		if err != nil {
-			return err
-		}
+	p.run(DefaultPublishFunc(p.env, pub, p.exit))
 
-		defer s.Close()
-
-		<-p.exit
-
-		// close channel
-		close(sub)
-
-		return nil
-	}
+	return pub
 }
 
+// Wait ...
 func (p *plugin) Wait() error {
 	<-p.exit
 
@@ -88,4 +84,55 @@ func (p *plugin) run(f func() error) {
 			})
 		}
 	}()
+}
+
+func pubFunc(env *Env, pub <-chan *pb.Event, exit <-chan struct{}) func() error {
+	return func() error {
+		sc, err := stan.Connect(env.Get(AutobotClusterID), env.Get(AutobotClusterURL))
+		if err != nil {
+			return err
+		}
+		defer sc.Close()
+
+		for {
+			select {
+			case _, ok := <-pub:
+				if !ok {
+					return nil
+				}
+
+				if err := sc.Publish(env.Get(AutobotTopic), []byte("")); err != nil {
+					return err
+				}
+			case <-exit:
+				return nil
+			}
+		}
+	}
+}
+
+func subFunc(env *Env, sub chan<- *pb.Event, exit <-chan struct{}) func() error {
+	return func() error {
+		sc, err := stan.Connect(env.Get(AutobotClusterID), env.Get(AutobotClusterURL))
+		if err != nil {
+			return err
+		}
+		defer sc.Close()
+
+		s, err := sc.Subscribe(env.Get(AutobotTopic), func(m *stan.Msg) {
+			sub <- &pb.Event{}
+		})
+		if err != nil {
+			return err
+		}
+
+		defer s.Close()
+
+		<-exit
+
+		// close channel
+		close(sub)
+
+		return nil
+	}
 }
