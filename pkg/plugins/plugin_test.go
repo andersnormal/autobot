a892bca4ea7c36b85ef576330ca101672c78395a
Andrew Louis <alouis@digitalocean.com>
2019-09-03 09:45:30 -0400

    Add OutboxPub and ReplyFunc tests

--- pkg/plugins/plugin_test.go
+++ pkg/plugins/plugin_test.go
@@ -2,42 +2,114 @@ package plugins
 
 import (
 	"context"
-	"log"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/andersnormal/autobot/pkg/plugins/runtime"
 	pb "github.com/andersnormal/autobot/proto"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/sync/errgroup"
 )
 
-func TestInboxPubSub(t *testing.T) {
+const (
+	waitTimeout = 3 * time.Second
+)
+
+var env = runtime.DefaultEnv().WithName("autobot").WithClusterURL("nats://localhost:4222")
+
+func TestInboxPub(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	env := runtime.DefaultEnv().WithName("test").WithClusterURL("nats://localhost:4222")
 	plugin, ctx := WithContext(ctx, env)
+
+	inboxMessageCh := make(chan string, 1)
+
+	plugin.ReplyWithFunc(func(msg *pb.Message) (*pb.Message, error) {
+		inboxMessageCh <- msg.GetText()
+		return nil, nil
+	})
 
 	plugin.PublishInbox() <- &pb.Message{
 		Text: "hello world",
 	}
 
-	var receivedInboxMessage string
-
-	err := plugin.ReplyWithFunc(func(msg *pb.Message) (*pb.Message, error) {
-		receivedInboxMessage = msg.GetText()
-		return nil, nil
-	})
-	if err != nil {
-		log.Fatal(err)
+	select {
+	case msg := <-inboxMessageCh:
+		assert.Equal(t, "hello world", msg)
+	case <-time.After(waitTimeout):
+		assert.FailNow(t, "timed out waiting for message...")
 	}
+}
 
-	assert.Eventually(t, func() bool {
-		if receivedInboxMessage == "hello world" {
-			return true
+func TestOutboxPub(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	plugin, ctx := WithContext(ctx, env)
+
+	outboxMessageCh := make(chan string, 1)
+
+	var g errgroup.Group
+	g.Go(func() error {
+		e := <-plugin.SubscribeOutbox()
+
+		switch ev := e.(type) {
+		case *pb.Message:
+			outboxMessageCh <- ev.GetText()
 		}
 
-		return false
-	}, 2*time.Second, 10*time.Millisecond)
+		return nil
+	})
+
+	plugin.PublishOutbox() <- &pb.Message{
+		Text: "hello world",
+	}
+
+	g.Wait()
+
+	select {
+	case msg := <-outboxMessageCh:
+		assert.Equal(t, "hello world", msg)
+	case <-time.After(waitTimeout):
+		assert.FailNow(t, "timed out waiting for message...")
+	}
+}
+
+func TestReplyFunc(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	plugin, ctx := WithContext(ctx, env)
+
+	outboxMessageCh := make(chan string, 1)
+
+	var g errgroup.Group
+	g.Go(func() error {
+		e := <-plugin.SubscribeOutbox()
+
+		switch ev := e.(type) {
+		case *pb.Message:
+			outboxMessageCh <- ev.GetText()
+		}
+
+		return nil
+	})
+
+	plugin.ReplyWithFunc(func(msg *pb.Message) (*pb.Message, error) {
+		return msg.Reply("reply message"), nil
+	})
+
+	plugin.PublishInbox() <- &pb.Message{
+		Text: "hello world",
+	}
+
+	g.Wait()
+
+	select {
+	case msg := <-outboxMessageCh:
+		assert.Equal(t, "reply message", msg)
+	case <-time.After(waitTimeout):
+		assert.FailNow(t, "timed out waiting for message...")
+	}
 }
